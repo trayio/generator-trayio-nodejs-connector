021ca64826f958bba21f1f8d9e161dc49febf703
Alistair Russell <ali@tray.io>
2015-11-11 11:48:01 +0000

    Update trigger command to pass new connector fields and also set conflicter force to true

--- generators/trigger/index.js
+++ generators/trigger/index.js
@@ -9,12 +9,16 @@ module.exports = generators.Base.extend({
 		this.connectorName = process.cwd().split(path.sep).pop();
 	},
 	createHttp: function() {
+		this.conflicter.force = true;
 	    this.fs.copyTpl(
 			this.templatePath("trigger.js"),
 			this.destinationPath(TRIGGER_FOLDER + "/trigger.js")
 	    );		
 		this.composeWith('trayio-nodejs-connector:connector', { options: {
 			name: this.connectorName + "-trigger",
+			title: "Trigger",
+			description: "Auto created connector trigger",
+			connectorType: "trigger",
 			help_link: "#"
 		}});		    
 		this.composeWith('trayio-nodejs-connector:message', { options: {
