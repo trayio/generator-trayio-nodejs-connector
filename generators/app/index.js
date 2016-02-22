aa5cbe30f3090180bd1c8c2509747cf818827fca
Chris Houghton <chrishoughton@Chriss-MacBook-Pro.local>
2016-02-22 11:09:18 +0000

    code formatting and tidying up

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,170 +5,159 @@ var slugify = require('mout/string/slugify');
 var OPERATION_FOLDER = "operations";
 
 module.exports = generators.Base.extend({
-	constructor: function () {
-		generators.Base.apply(this, arguments);		
+	constructor: function() {
+		generators.Base.apply(this, arguments);
 		this.connectorName = process.cwd().split(path.sep).pop();
-	},	
+	},
 	promptProjectTitle: function() {
 		var done = this.async();
 		this.prompt({
-			type    : 'input',
-			name    : 'title',
-			message : 'Connector title (as it will appear in the tray UI)',
-			default : this.appname // Default to current folder name
-		}, function (answers) {
+			type: 'input',
+			name: 'title',
+			message: 'Connector title (as it will appear in the tray UI)',
+			default: this.appname // Default to current folder name
+		}, function(answers) {
 			this.log(answers.title);
-			this.title = answers.title;			
+			this.title = answers.title;
 			done();
-		}.bind(this));		
+		}.bind(this));
 	},
 	promptProjectName: function() {
 		var done = this.async();
 		this.prompt({
-			type    : 'input',
-			name    : 'name',
-			message : 'Connector name (how it will be referenced in workflows)',
-			default : slugify(this.title) // Default to current folder name
-		}, function (answers) {
+			type: 'input',
+			name: 'name',
+			message: 'Connector name (how it will be referenced in workflows)',
+			default: slugify(this.title) // Default to current folder name
+		}, function(answers) {
 			this.log(answers.name);
-			this.name = answers.name;			
+			this.name = answers.name;
 			done();
-		}.bind(this));		
+		}.bind(this));
 	},
 	promptProjectDescription: function() {
 		var done = this.async();
 		this.prompt({
-			type    : 'input',
-			name    : 'description',
-			message : 'Description'
-		}, function (answers) {
+			type: 'input',
+			name: 'description',
+			message: 'Description'
+		}, function(answers) {
 			this.log(answers.description);
-			this.description = answers.description;			
+			this.description = answers.description;
 			done();
-		}.bind(this));		
+		}.bind(this));
 	},
 	promptAuthor: function() {
 		var done = this.async();
 		this.prompt({
-			type    : 'input',
-			name    : 'author',
-			message : 'Author'
-		}, function (answers) {
+			type: 'input',
+			name: 'author',
+			message: 'Author'
+		}, function(answers) {
 			this.log(answers.author);
-			this.author = answers.author;			
+			this.author = answers.author;
 			done();
-		}.bind(this));		
+		}.bind(this));
 	},
 	promptRepo: function() {
 		var done = this.async();
 		this.prompt({
-			type    : 'input',
-			name    : 'repository',
-			message : 'Repository'
-		}, function (answers) {
+			type: 'input',
+			name: 'repository',
+			message: 'Repository'
+		}, function(answers) {
 			this.log(answers.repository);
-			this.repository = answers.repository;			
+			this.repository = answers.repository;
 			done();
-		}.bind(this));		
-	},	
+		}.bind(this));
+	},
 	promptIncludeHttpTrigger: function() {
 		var done = this.async();
 		this.prompt({
-			type    : 'confirm',
-			name    : 'httpTrigger',
-			message : 'Include an HTTP trigger?',
-			default : true
-		}, function (answers) {
+			type: 'confirm',
+			name: 'httpTrigger',
+			message: 'Include an HTTP trigger?',
+			default: true
+		}, function(answers) {
 			this.log(answers.httpTrigger);
-			this.includeHttpTrigger = answers.httpTrigger;			
+			this.includeHttpTrigger = answers.httpTrigger;
 			done();
-		}.bind(this));	
-	},	
+		}.bind(this));
+	},
 	createPackage: function() {
-	    this.fs.copyTpl(
+		this.fs.copyTpl(
 			this.templatePath("package.json"),
-			this.destinationPath("package.json"),
-			{
+			this.destinationPath("package.json"), {
 				name: this.connectorName,
 				description: this.description,
 				author: this.author,
 				repository: this.repository
 			}
-	    );		
+		);
 	},
 	copyFiles: function() {
 
 		//copy .files
-		this.fs.copyTpl(this.templatePath(".editorconfig"),this.destinationPath(".editorconfig"),{});	
-		this.fs.copyTpl(this.templatePath(".eslintignore"),this.destinationPath(".eslintignore"),{});	
-		this.fs.copyTpl(this.templatePath(".eslintrc"),this.destinationPath(".eslintrc"),{});	
-		this.fs.copyTpl(this.templatePath(".gitignore"),this.destinationPath(".gitignore"),{});	
-		this.fs.copyTpl(this.templatePath(".jsinspectrc"),this.destinationPath(".jsinspectrc"),{});	
-		this.fs.copyTpl(this.templatePath(".travis.yml"),this.destinationPath(".travis.yml"),{});	
-		this.fs.copyTpl(this.templatePath("Gruntfile.js"),this.destinationPath("Gruntfile.js"),{});	
+		this.fs.copyTpl(this.templatePath(".editorconfig"), this.destinationPath(".editorconfig"), {});
+		this.fs.copyTpl(this.templatePath(".eslintignore"), this.destinationPath(".eslintignore"), {});
+		this.fs.copyTpl(this.templatePath(".eslintrc"), this.destinationPath(".eslintrc"), {});
+		this.fs.copyTpl(this.templatePath(".gitignore"), this.destinationPath(".gitignore"), {});
+		this.fs.copyTpl(this.templatePath(".jsinspectrc"), this.destinationPath(".jsinspectrc"), {});
+		this.fs.copyTpl(this.templatePath(".travis.yml"), this.destinationPath(".travis.yml"), {});
+		this.fs.copyTpl(this.templatePath("Gruntfile.js"), this.destinationPath("Gruntfile.js"), {});
 
-	    this.fs.copyTpl(this.templatePath("README.md"),this.destinationPath("README.md"),
-	    {
-	    	name: this.name,
-	    	description: this.description,
-	    });	
-	    this.fs.copyTpl(this.templatePath("yart_run.sh"),this.destinationPath("yart_run.sh"),{});	
+		this.fs.copyTpl(this.templatePath("README.md"), this.destinationPath("README.md"), {
+			name: this.name,
+			description: this.description,
+		});
+		this.fs.copyTpl(this.templatePath("yart_run.sh"), this.destinationPath("yart_run.sh"), {});
 
-	},	
+	},
 	createConnectorJSON: function() {
 		this.fs.write(this.destinationPath("connectors.json"), JSON.stringify([]));
 	},
 	installSDKDependency: function() {
-		this.npmInstall(['@trayio/falafel'], { 'save': true });		
-	},	
-	installGruntDependency: function () {
-		this.npmInstall(['grunt', 'grunt-contrib-jshint', 'grunt-contrib-watch'], { 'saveDev': true });
+		this.npmInstall(['@trayio/falafel'], {
+			'save': true
+		});
 	},
-
-	createConnectorModule: function () {
-
+	installGruntDependency: function() {
+		this.npmInstall(['grunt', 'grunt-contrib-jshint', 'grunt-contrib-watch'], {
+			'saveDev': true
+		});
 	},
 	createMain: function() {
-	    this.fs.copyTpl(
+		this.fs.copyTpl(
 			this.templatePath("main.js"),
-			this.destinationPath("main.js"),
-			{
+			this.destinationPath("main.js"), {
 				title: this.title
 			}
-	    );		
+		);
 	},
-	createConnectorsFolder: function () {
-		console.log('creating connectors folder');
-
-		this.fs.copyTpl(this.templatePath("connector/connector.js"),this.destinationPath('connectors/'+this.name+'/connector.js'),{
+	createConnectorsFolder: function() {
+		this.fs.copyTpl(this.templatePath("connector/connector.js"), this.destinationPath('connectors/' + this.name + '/connector.js'), {
 			title: this.title,
 			description: this.description
 		});
 
-		this.fs.copyTpl(this.templatePath('connector/global.js'), this.destinationPath('connectors/'+this.name+'/global.js'), {});
+		this.fs.copyTpl(this.templatePath('connector/global.js'), this.destinationPath('connectors/' + this.name + '/global.js'), {});
 
 		if (this.includeHttpTrigger) {
-			this.fs.copyTpl(this.templatePath('connector/trigger.js'), this.destinationPath('connectors/'+this.name+'/trigger.js'), {});
+			this.fs.copyTpl(this.templatePath('connector/trigger.js'), this.destinationPath('connectors/' + this.name + '/trigger.js'), {});
 		}
 	},
-	createSampleMessage: function () {
+	createSampleMessage: function() {
 		this.fs.copyTpl(
-			this.templatePath('connector/sample_message/model.js'), 
-			this.destinationPath('connectors/'+this.name+'/sample_message/model.js'), {}
+			this.templatePath('connector/sample_message/model.js'),
+			this.destinationPath('connectors/' + this.name + '/sample_message/model.js'), {}
 		);
 		this.fs.copyTpl(
-			this.templatePath('connector/sample_message/schema.js'), 
-			this.destinationPath('connectors/'+this.name+'/sample_message/schema.js'), {}
+			this.templatePath('connector/sample_message/schema.js'),
+			this.destinationPath('connectors/' + this.name + '/sample_message/schema.js'), {}
 		);
 		this.fs.copyTpl(
-			this.templatePath('connector/sample_message/response.sample.json'), 
-			this.destinationPath('connectors/'+this.name+'/sample_message/response.sample.json'), {}
+			this.templatePath('connector/sample_message/response.sample.json'),
+			this.destinationPath('connectors/' + this.name + '/sample_message/response.sample.json'), {}
 		);
 	}
-	// createHttp: function() {
-	// 	if (this.includeHttpTrigger) {		  
-	// 		this.composeWith("trayio-nodejs-connector:trigger", { options: {				
-	// 		}});		  
-	// 	}
-	// }
 });
