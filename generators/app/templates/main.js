a7513750ce962d163786f007961634573b456580
Taniya Amidon <taniyaamidon@taniyas-mbp.home>
2020-03-12 15:32:06 +0000

    change var to const in main.js

--- generators/app/templates/main.js
+++ generators/app/templates/main.js
@@ -1,6 +1,6 @@
-var Falafel = require('@trayio/falafel');
+const Falafel = require('@trayio/falafel');
 // Set up the lambda function by wrapping the current directory
-var apptalk = new Falafel().wrap({
+const apptalk = new Falafel().wrap({
 	directory: `${__dirname}/`,
 });
 
