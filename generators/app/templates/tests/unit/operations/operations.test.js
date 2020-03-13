f48bdb240b4434e526389504a22fbd43eddc26ed
Taniya Amidon <taniyaamidon@taniyas-mbp.home>
2020-03-13 14:32:13 +0000

    fix the path in test runner to point to root directory

--- generators/app/templates/tests/unit/operations/operations.test.js
+++ generators/app/templates/tests/unit/operations/operations.test.js
@@ -1,2 +1,6 @@
 //See https://github.com/trayio/connector-test-runner
-require('@trayio/connector-test-runner').run(__dirname);
+require('@trayio/connector-test-runner').run(
+	`${__dirname}`,
+	`${__dirname}/../../../`,
+);
+
